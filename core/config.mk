abcc987e24dfb22a1aa09e6092c1e50a045a6041
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-02-06 17:12:44 +0000

    Update config.mk

--- core/config.mk
+++ core/config.mk
@@ -458,7 +458,7 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-include vendor/omni/config/BoardConfigOmni.mk
+include vendor/twrp/config/BoardConfigTWRP.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
