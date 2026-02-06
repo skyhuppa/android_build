7f8fe0b1d125083191e11b52c2031e524686b8d1
Adasi Kaakyire <Skyhuppa@gmail.com>
2026-02-06 19:20:53 +0000

    Update config.mk

--- core/config.mk
+++ core/config.mk
@@ -866,7 +866,7 @@ ifdef PRODUCT_SHIPPING_API_LEVEL
 endif
 
 # The default key if not set as LOCAL_CERTIFICATE
-include $(TOPDIR)vendor/omni/build/core/certs.mk
+include $(TOPDIR)vendor/twrp/build/core/certs.mk
 
 ifdef PRODUCT_DEFAULT_DEV_CERTIFICATE
   DEFAULT_SYSTEM_DEV_CERTIFICATE := $(PRODUCT_DEFAULT_DEV_CERTIFICATE)
@@ -1324,7 +1324,7 @@ SOONG_VARIABLES :=
 SOONG_EXTRA_VARIABLES :=
 
 # Rules for QCOM targets
-include vendor/omni/build/core/qcom_target.mk
+include vendor/twrp/build/core/qcom_target.mk
 
 include $(BUILD_SYSTEM)/dumpvar.mk
 
